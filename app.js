d3478965815f2047b0d01fedb70667342008b86a
kangpuqing <249740672@qq.com>
2019-04-08 17:48:03 +0800

    next

--- app.js
+++ app.js
@@ -3,18 +3,36 @@ const Router = require('koa-router');
 const static = require('koa-static')
 const body = require('koa-better-body')
 const session = require('koa-session')
+const mysql = require('mysql')
+const co = require('co-mysql')
+
+// let conn = mysql.createPool({
+//     host:'localhost',
+//     user:'root',
+//     database:'20181101'
+// })
+
+// let db = co(conn)
 
 let server = new Koa();
 server.listen(8000);
+
+// server.context.db = db;
 
 server.use(body({
     uploadDir:'./static/upload'
 }))
 
-// server.use(async ctx=>{
-//     //文件和post数据
-//     console.log(ctx.request)
-// })
+server.use(session({
+    maxAge:20*60*1000,      //有效期
+    renew:true      //自动续期
+},server))
+
+server.use(async (ctx,next)=>{
+    //文件和post数据
+    console.log(ctx.request)
+    next()
+})
 
 server.context.a = 12;
 
